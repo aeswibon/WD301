785c80f3985060184089b9f1767bb0a41793d532
cp-Coder <bt20mme002@students.vnit.ac.in>
2022-03-21 01:37:14 +0530

    added array answers and question number in preview form

--- src/Components/Preview.tsx
+++ src/Components/Preview.tsx
@@ -2,7 +2,7 @@ import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { navigate } from "raviger";
 import React from "react";
-import { formDataChecker } from "../interfaces/form";
+import { formDataChecker, formFieldChecker } from "../interfaces/form";
 import { getLocalForms } from "../utils/form";
 
 const findForm = (formId: string): formDataChecker => {
@@ -32,17 +32,27 @@ const getInitialState = (formId: string, questionId: number) => {
 };
 
 const getInitialAnswerState = (form: formDataChecker) => {
-	const answers: string[] = [];
+	const answers: formFieldChecker[] = [];
 	form.formFields.forEach((field, idx) => {
-		answers[idx] = "";
+		answers[idx] = {
+			id: "",
+			label: "",
+			type: "",
+			value: "",
+		};
 	});
 	return answers;
 };
+
 const PreviewForm = (props: { formId: string }): JSX.Element => {
-	const [state, setState] = React.useState(getInitialState(props.formId, 0));
+	const [state, setState] = React.useState(() =>
+		getInitialState(props.formId, 0),
+	);
 	const { form, question } = state;
 	const [questionId, setQuestionId] = React.useState(state.questionId);
-	const [answers, setAnswers] = React.useState(getInitialAnswerState(form));
+	const [answers, setAnswers] = React.useState<formFieldChecker[]>(() =>
+		getInitialAnswerState(form),
+	);
 
 	React.useEffect(() => {
 		setState(getInitialState(props.formId, questionId));
@@ -50,14 +60,32 @@ const PreviewForm = (props: { formId: string }): JSX.Element => {
 
 	const handleAnswer = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const newAns = [...answers];
-		newAns[questionId] = e.target.value;
+		newAns[questionId] = {
+			id: question?.id,
+			label: question?.label,
+			type: question?.type,
+			value: e.target.value,
+		};
 		setAnswers(newAns);
+	};
+	const handleSubmit = () => {
+		let msg = "Hello User,\n";
+		msg += "Your answers has been recorded.\n";
+		msg += `Form title: ${form.title}\n\n`;
+		answers.forEach((answer) => {
+			msg += `${answer.label}: ${answer.value}\n`;
+		});
+		msg += "\nThank you for your time.";
+		alert(msg);
 	};
 
 	return (
-		<div className="flex flex-col flex-1">
-			<div className="flex w-full my-3 justify-between">
-				<h2 className="text-3xl font-semibold">Preview</h2>
+		<div className="flex flex-col w-full">
+			<div className="flex my-3 justify-between">
+				<h2 className="text-3xl font-semibold">{form.title}</h2>
+				<span>
+					Question {questionId + 1}/{form.formFields.length}
+				</span>
 			</div>
 			<div className="flex flex-col">
 				<div>
@@ -70,10 +98,11 @@ const PreviewForm = (props: { formId: string }): JSX.Element => {
 						name={question?.label}
 						placeholder={question?.label}
 						type={question?.type}
-						value={answers[questionId]}
+						value={answers[questionId].value}
 						onChange={handleAnswer}
 					/>
 				</div>
+				{console.log(answers)}
 				<div className="flex justify-end w-full gap-2">
 					{questionId > 0 && (
 						<button
@@ -93,6 +122,13 @@ const PreviewForm = (props: { formId: string }): JSX.Element => {
 							}
 							className="bg-rose-500 text-white font-bold py-2 px-4 my-4 rounded">
 							Next <FontAwesomeIcon icon={faArrowRight} />
+						</button>
+					)}
+					{questionId === form.formFields.length - 1 && (
+						<button
+							onClick={handleSubmit}
+							className="bg-rose-500 text-white font-bold py-2 px-4 my-4 rounded">
+							Submit
 						</button>
 					)}
 				</div>
