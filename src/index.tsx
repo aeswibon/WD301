7736d443d9a61f11b8a0dc5fd9a2eb6f062a9b13
cp-Coder <bt20mme002@students.vnit.ac.in>
2022-04-12 21:06:47 +0530

    modified files for production build

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+if (process.env.REACT_APP_SENTRY_DSN) {
+	Sentry.init({
+		dsn: process.env.REACT_APP_SENTRY_DSN,
+		integrations: [new BrowserTracing()],
+		tracesSampleRate: 1.0,
+	});
+}
 
 ReactDOM.render(
 	<React.StrictMode>
